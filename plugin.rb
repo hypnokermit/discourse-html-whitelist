7e38633d083298aa2062e682c3a9a0bf6514292a
Caswal <caswal@camshaftsoftware.com>
2022-06-20 10:23:45 +1200

    Update plugin.rb
    Updated Version Number

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-html-whitelist
 # about: Basic template to allow different html elements and attributes when creating a class.
-# version: 0.0.2
+# version: 0.0.3
 # authors: Paul Newell, Caswal Parker
 # url: https://github.com/caswal/discourse-html-whitelist
 
