54926fc344760aa531ce925ac97d48e88c2c0a30
hypnokermit <108853667+hypnokermit@users.noreply.github.com>
2022-07-11 15:21:30 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-html-whitelist
-# about: Basic template to allow different html elements and attributes when creating a class.
-# version: 0.0.4
-# authors: Paul Newell, Caswal Parker
-# url: https://github.com/caswal/discourse-html-whitelist
+# about: Allows classes on all tags, and inline styles on select tags.
+# version: 0.0.5
+# authors: Paul Newell, Caswal Parker, hypnokermit
+# url: https://github.com/hypnokermit/discourse-html-whitelist
 
 #register_asset "javascript/div.js.es6", :server_side
